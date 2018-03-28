fae7195e859a82470dc98f1162bc631b72054ca8
Higherwill <37810466+Higherwill@users.noreply.github.com>
2018-03-28 14:46:06 -0300

    Add files via upload

---
+++ Exercicios pag 19/Exercício 15.py
@@ -0,0 +1,10 @@
+#Tentativa de resolução utilizando o método de semelhança de triângulos
+your_height=float(input("Meça sua altura com a trena e insira o valor "))
+your_shadow=float(input("Insira o comprimento da sombra "))
+object_shadow=float(input("Meça o comprimento da sombra da caixa d'agua "))
+object_height=your_height*object_shadow/your_shadow
+print("A altura da caixa d'água é", object_height, "metros")
+
+                  
+                  
+                  
